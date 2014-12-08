c449422ae770ad29146b9b957f13ba2d96253e18
Ryan <prk223@gmail.com>
2014-12-08 00:50:39 +0000

    Added a variable def to the DatumDefTest to test both parts of the struct as well as multiple things in the PduDef vector.

--- test/DatumDefTest.cpp
+++ test/DatumDefTest.cpp
@@ -15,7 +15,8 @@ class DatumDefTest : public testing::Test
 public:
     BaseDef* base_entity;
     BaseDef* base_fixed;
-    BaseDef* base_variable;
+    BaseDef* base_variable1;
+    BaseDef* base_variable2;
     KDIS::PDU::Entity_State_PDU entity_pdu;
     KDIS::PDU::Data_PDU fixed_pdu;
     KDIS::PDU::Set_Data_PDU variable_pdu;
@@ -29,7 +30,8 @@ public:
     const std::string VARIABLE_NAME, VARIABLE_CATEGORY, VARIABLE_DESCRIPTION;
     const SenderId SENDER_ENTITY, SENDER_FIXED, SENDER_VARIABLE;
     const uint32_t FIXED_VALUE;
-    const double   VARIABLE_VALUE;
+    const uint16_t VARIABLE_INT;
+    const double   VARIABLE_DOUBLE;
     const std::string FIXED_UNIT, VARIABLE_UNIT;
 
     DatumDefTest() :
@@ -49,13 +51,15 @@ public:
         SENDER_FIXED(14, 99, 455),
         SENDER_VARIABLE(1, 0, 0),
         FIXED_VALUE(586738234),
-        VARIABLE_VALUE(-55.6),
+        VARIABLE_INT(493),
+        VARIABLE_DOUBLE(-55.6),
         FIXED_UNIT("m"),
         VARIABLE_UNIT("radians/s")
     {
-        base_entity   = NULL;
-        base_fixed    = NULL;
-        base_variable = NULL;
+        base_entity    = NULL;
+        base_fixed     = NULL;
+        base_variable1 = NULL;
+        base_variable2 = NULL;
     }
 
     virtual ~DatumDefTest(){}
@@ -143,21 +147,37 @@ protected:
         idVariable.setPduType(KDIS::DATA_TYPE::ENUMS::Set_Data_PDU_Type);
         idVariable.setFixedType(0);
         idVariable.setVariableType(VARIABLE_TYPE);
-        base_variable = new BaseDef();
-        base_variable->setDefinitionId(idVariable);
+        base_variable1 = new BaseDef();
+        base_variable1->setDefinitionId(idVariable);
 
-        base_variable->setLength(8);
-        base_variable->setOffset(2);
-        base_variable->setByteOrder("network");
-        base_variable->setType("double");
-        base_variable->setUnit(VARIABLE_UNIT);
-        base_variable->setUnitClass("");
-        base_variable->setName(VARIABLE_NAME);
-        base_variable->setCategory(VARIABLE_CATEGORY);
-        base_variable->setDescription(VARIABLE_DESCRIPTION);
-        base_variable->setMin(NULL);    // TODO
-        base_variable->setMax(NULL);    // TODO
-        base_variable->setEnumType(""); // TODO
+        base_variable1->setLength(8);
+        base_variable1->setOffset(2);
+        base_variable1->setByteOrder("network");
+        base_variable1->setType("double");
+        base_variable1->setUnit(VARIABLE_UNIT);
+        base_variable1->setUnitClass("");
+        base_variable1->setName(VARIABLE_NAME);
+        base_variable1->setCategory(VARIABLE_CATEGORY);
+        base_variable1->setDescription(VARIABLE_DESCRIPTION);
+        base_variable1->setMin(NULL);    // TODO
+        base_variable1->setMax(NULL);    // TODO
+        base_variable1->setEnumType(""); // TODO
+
+        base_variable2 = new BaseDef();
+        base_variable2->setDefinitionId(idVariable);
+
+        base_variable2->setLength(2);
+        base_variable2->setOffset(0);
+        base_variable2->setByteOrder("network");
+        base_variable2->setType("uint16");
+        base_variable2->setUnit(VARIABLE_UNIT);
+        base_variable2->setUnitClass("");
+        base_variable2->setName(VARIABLE_NAME);
+        base_variable2->setCategory(VARIABLE_CATEGORY);
+        base_variable2->setDescription(VARIABLE_DESCRIPTION);
+        base_variable2->setMin(NULL);    // TODO
+        base_variable2->setMax(NULL);    // TODO
+        base_variable2->setEnumType(""); // TODO
     }
 
     void SetUpVariablePdu()
@@ -172,14 +192,15 @@ protected:
         #pragma pack(1)
         struct VarTestStruct
         {
-            uint16_t unused;
-            double   test_value;
+            uint16_t test_int;
+            double   test_double;
         } test;
         #pragma pack()
 
-        uint64_t* valPtr = (uint64_t*)&VARIABLE_VALUE;
+        test.test_int = __builtin_bswap16(VARIABLE_INT);
+        uint64_t* valPtr = (uint64_t*)&VARIABLE_DOUBLE;
         uint64_t  val = __builtin_bswap64(*valPtr);
-        valPtr = (uint64_t*)(&(test.test_value));
+        valPtr = (uint64_t*)(&(test.test_double));
         *valPtr = val;
         const char* testPtr = (char*)&test;
 
@@ -205,8 +226,10 @@ protected:
             delete base_entity;
         if(base_fixed != NULL)
             delete base_fixed;
-        if(base_variable != NULL)
-            delete base_variable;
+        if(base_variable1 != NULL)
+            delete base_variable1;
+        if(base_variable2 != NULL)
+            delete base_variable2;
     }
 
 };
@@ -268,10 +291,12 @@ TEST_F(DatumDefTest, VariableDefTest)
     unsigned char* data = (unsigned char *)pduStream.GetBufferPtr();
     std::vector<DatumInfo*> datums;
     PduDef pduDef;
-    pduDef.add(base_variable);
-    base_variable = NULL; // Let PduDef handle destruction
+    pduDef.add(base_variable1);
+    pduDef.add(base_variable2);
+    base_variable1 = NULL; // Let PduDef handle destruction
+    base_variable2 = NULL; // Let PduDef handle destruction
     pduDef.getDatums(&variable_pdu, data, bufSize, &datums);
-    ASSERT_EQ(1, datums.size());
+    ASSERT_EQ(2, datums.size());
 
     DatumInfo* datum = datums[0];
     DatumIdentifier sender = datum->getId();
@@ -282,10 +307,26 @@ TEST_F(DatumDefTest, VariableDefTest)
     EXPECT_EQ(VARIABLE_CATEGORY,      datum->getCategory());
     EXPECT_EQ(VARIABLE_DESCRIPTION,   datum->getDescription());
 
-    std::ostringstream ss;
-    ss << VARIABLE_VALUE;
-    std::string val = ss.str();
+    std::ostringstream ss1;
+    ss1 << VARIABLE_DOUBLE;
+    std::string val = ss1.str();
     EXPECT_EQ(val, datum->getValue());
+    delete datum;
+
+    datum = datums[1];
+    sender = datum->getId();
+    EXPECT_EQ(SENDER_VARIABLE.site,   sender.getSite());
+    EXPECT_EQ(SENDER_VARIABLE.app,    sender.getApp());
+    EXPECT_EQ(SENDER_VARIABLE.entity, sender.getEntity());
+    EXPECT_EQ(VARIABLE_NAME,          datum->getName());
+    EXPECT_EQ(VARIABLE_CATEGORY,      datum->getCategory());
+    EXPECT_EQ(VARIABLE_DESCRIPTION,   datum->getDescription());
+
+    std::ostringstream ss2;
+    ss2 << VARIABLE_INT;
+    val = ss2.str();
+    EXPECT_EQ(val, datum->getValue());
+    delete datum;
 }
 
 
